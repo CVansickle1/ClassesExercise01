f1f7199b95cff90c18dd344d1663fd2f4fc0b027
Cody <Cojo341@gmail.com>
2022-08-18 13:48:08 -0400

    Initial Commit

---
+++ ClassExercise01/Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise01
+{
+    internal class Car
+    {
+        public Car()
+        {
+
+        }
+        
+        public string? Make { get; set; }
+
+        public string? Model { get; set; }
+        
+        public int Year { get; set; }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"The Make of Your Car is: {Make}");
+            Console.WriteLine($"The Model of Your Car is: {Model}");
+            Console.WriteLine($"The Year of Your Car is: {Year}");
+        }
+
+       
+
+    }
+    public class GettingInput
+    {
+        public static int GetYear()
+        {
+            bool numCheck;
+            Console.WriteLine("Please enter your Year: ");
+            numCheck = int.TryParse(Console.ReadLine(), out int num);
+
+            while (!numCheck)
+            {
+                Console.WriteLine("Please enter your Year: ");
+                numCheck = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            return num;
+        }
+
+        public static string? GetStringInput(int Reference)
+        {
+
+            string? userInput;
+            bool nullCheck;
+
+            if (Reference == 1)
+            {
+                Console.WriteLine("Please enter the Make of your vehicle: ");
+                userInput = Console.ReadLine();
+                nullCheck = string.IsNullOrEmpty(userInput);
+
+                while (nullCheck)
+                {
+                    Console.WriteLine("Please enter the Make of your vehicle: ");
+                    userInput = Console.ReadLine();
+                    nullCheck = string.IsNullOrEmpty(userInput);
+                }
+            }
+            else
+            {
+          
+                Console.WriteLine("Please enter the model of your vehicle: ");
+
+                userInput = Console.ReadLine();
+                nullCheck = string.IsNullOrEmpty(userInput);
+
+                while (nullCheck)
+                {
+                    Console.WriteLine("Please enter the model of your vehicle: ");
+                    userInput = Console.ReadLine();
+                    nullCheck = string.IsNullOrEmpty(userInput);
+                }
+            }
+            return userInput;
+        }
+    }
+}
---
+++ ClassExercise01/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassExercise01;
+
+Console.WriteLine("Please enter the Make, Model, and Year of your Vehicle: ");
+Car yourCar = new Car()
+{
+    Make = GettingInput.GetStringInput(1),
+    Model = GettingInput.GetStringInput(2),
+    Year = GettingInput.GetYear(),
+};
+
+yourCar.PrintCarInfo();
+
+
+
+
+
+
+
